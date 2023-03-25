7c7d797c0340231aa980cc800203f6ad2a885258
Denys <den.popov1002@gmail.com>
2023-03-26 00:32:53 +0200

    portfolio reviews

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,20 @@
-$(function(){
+$(function () {
+    const swiper = new Swiper('.swiper', {
 
+        
+        slidesPerView: 1,
+        mousewheel: true,
+        spaceBetween: 35,
+        centeredSlides: true,
+
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-prev',
+            prevEl: '.swiper-button-next',
+        },
+
+        // And if we need scrollbar
+
+    });
 });
