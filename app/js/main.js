76b4f26654cf7a970732917e4068c23d4c895117
Denys <den.popov1002@gmail.com>
2023-03-27 18:38:53 +0300

    Swiper-js

--- app/js/main.js
+++ app/js/main.js
@@ -1,58 +1,78 @@
-const tabBtn = document.querySelectorAll('.tab-btn');
-const tabsItems = document.querySelectorAll('.tab-item');
 
-$(function () {
-    const swiper = new Swiper('.swiper', {
-
-
-        slidesPerView: 1,
-        mousewheel: true,
-        spaceBetween: 35,
-        centeredSlides: true,
-
-
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-prev',
-            prevEl: '.swiper-button-next',
-        },
-
-        // And if we need scrollbar
-
+    const swiperContainer = document.querySelector('.swiper-container');
+    const swiperWrapper = swiperContainer.querySelector('.swiper-wrapper');
+    const swiperSlides = swiperWrapper.querySelectorAll('.swiper-slide');
+    const swiperPrevButton = swiperContainer.querySelector('.swiper-button-prev');
+    const swiperNextButton = swiperContainer.querySelector('.swiper-button-next');
+    let currentSlide = 0;
+    
+    // встановлюємо активним перший слайд
+    swiperSlides[currentSlide].classList.add('swiper-slide-active');
+    
+    // обробник кнопки "назад"
+    swiperPrevButton.addEventListener('click', () => {
+      swiperSlides[currentSlide].classList.remove('swiper-slide-active');
+      currentSlide--;
+      if (currentSlide < 0) {
+        currentSlide = swiperSlides.length - 1;
+      }
+      swiperSlides[currentSlide].classList.add('swiper-slide-active');
     });
-});
+    
+    // обробник кнопки "вперед"
+    swiperNextButton.addEventListener('click', () => {
+      swiperSlides[currentSlide].classList.remove('swiper-slide-active');
+      currentSlide++;
+      if (currentSlide >= swiperSlides.length) {
+        currentSlide = 0;
+      }
+      swiperSlides[currentSlide].classList.add('swiper-slide-active');
+    });
+    
+    // автоматична зміна слайдів
+    // setInterval(() => {
+    //   swiperSlides[currentSlide].classList.remove('swiper-slide-active');
+    //   currentSlide++;
+    //   if (currentSlide >= swiperSlides.length) {
+    //     currentSlide = 0;
+    //   }
+    //   swiperSlides[currentSlide].classList.add('swiper-slide-active');
+    // }, 5000);
+    
+    
 
 
-tabBtn.forEach(onTabClick)
 
-function onTabClick(item) {
-    item.addEventListener("click", function () {
-        let currentBtn = item;
-        let tabId = currentBtn.getAttribute("data-tab")
-        let currentTab = document.querySelector(tabId)
+    const tabBtn = document.querySelectorAll('.tab-btn');
+    const tabsItems = document.querySelectorAll('.tab-item');
+
+    tabBtn.forEach(onTabClick)
+
+    function onTabClick(item) {
+        item.addEventListener("click", function () {
+            let currentBtn = item;
+            let tabId = currentBtn.getAttribute("data-tab");
+            let currentTab = document.querySelector(tabId);
 
 
-        if (!currentBtn.classList.contains('active')) {
-            tabBtn.forEach(function (item) {
-                item.classList.remove('active')
-            });
-            tabsItems.forEach(function (item) {
-                item.classList.remove('active')
-            });
+            if (currentBtn.classList.contains("active")) {
+                currentBtn.classList.remove("active");
+                currentTab.classList.remove("active");
+            } else {
+                tabBtn.forEach(function (item) {
+                    item.classList.remove("active");
+                });
+                tabsItems.forEach(function (item) {
+                    item.classList.remove("active");
+                });
 
-            currentBtn.classList.add('active')
-            currentTab.classList.add('active')
-        }
-        currentBtn.addEventListener('click', () => {
-            currentBtn.classList.toggle('active');
-            currentTab.classList.toggle('active');
+                currentBtn.classList.add("active");
+                currentTab.classList.add("active");
+            }
         });
-
-
-    });
+    }
 
 
 
 
-}
 
