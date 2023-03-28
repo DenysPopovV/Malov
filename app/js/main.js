4a423737368f1b18c227a9b8382b61bb1bb60634
PavloPopovV <pavlopopov570@gmail.com>
2023-03-28 23:19:47 +0300

    services

--- app/js/main.js
+++ app/js/main.js
@@ -1,78 +1,103 @@
 
-    const swiperContainer = document.querySelector('.swiper-container');
-    const swiperWrapper = swiperContainer.querySelector('.swiper-wrapper');
-    const swiperSlides = swiperWrapper.querySelectorAll('.swiper-slide');
-    const swiperPrevButton = swiperContainer.querySelector('.swiper-button-prev');
-    const swiperNextButton = swiperContainer.querySelector('.swiper-button-next');
-    let currentSlide = 0;
-    
-    // встановлюємо активним перший слайд
-    swiperSlides[currentSlide].classList.add('swiper-slide-active');
-    
-    // обробник кнопки "назад"
-    swiperPrevButton.addEventListener('click', () => {
-      swiperSlides[currentSlide].classList.remove('swiper-slide-active');
-      currentSlide--;
-      if (currentSlide < 0) {
-        currentSlide = swiperSlides.length - 1;
-      }
-      swiperSlides[currentSlide].classList.add('swiper-slide-active');
-    });
-    
-    // обробник кнопки "вперед"
-    swiperNextButton.addEventListener('click', () => {
-      swiperSlides[currentSlide].classList.remove('swiper-slide-active');
-      currentSlide++;
-      if (currentSlide >= swiperSlides.length) {
-        currentSlide = 0;
-      }
-      swiperSlides[currentSlide].classList.add('swiper-slide-active');
-    });
-    
-    // автоматична зміна слайдів
-    // setInterval(() => {
-    //   swiperSlides[currentSlide].classList.remove('swiper-slide-active');
-    //   currentSlide++;
-    //   if (currentSlide >= swiperSlides.length) {
-    //     currentSlide = 0;
-    //   }
-    //   swiperSlides[currentSlide].classList.add('swiper-slide-active');
-    // }, 5000);
-    
-    
+const swiperContainer = document.querySelector('.swiper-container');
+const swiperWrapper = swiperContainer.querySelector('.swiper-wrapper');
+const swiperSlides = swiperWrapper.querySelectorAll('.swiper-slide');
+const swiperPrevButton = swiperContainer.querySelector('.swiper-button-prev');
+const swiperNextButton = swiperContainer.querySelector('.swiper-button-next');
+let currentSlide = 0;
+
+swiperSlides[currentSlide].classList.add('swiper-slide-active');
+
+swiperPrevButton.addEventListener('click', () => {
+  swiperSlides[currentSlide].classList.remove('swiper-slide-active');
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = swiperSlides.length - 1;
+  }
+  swiperSlides[currentSlide].classList.add('swiper-slide-active');
+});
+
+swiperNextButton.addEventListener('click', () => {
+  swiperSlides[currentSlide].classList.remove('swiper-slide-active');
+  currentSlide++;
+  if (currentSlide >= swiperSlides.length) {
+    currentSlide = 0;
+  }
+  swiperSlides[currentSlide].classList.add('swiper-slide-active');
+});
 
 
 
-    const tabBtn = document.querySelectorAll('.tab-btn');
-    const tabsItems = document.querySelectorAll('.tab-item');
-
-    tabBtn.forEach(onTabClick)
-
-    function onTabClick(item) {
-        item.addEventListener("click", function () {
-            let currentBtn = item;
-            let tabId = currentBtn.getAttribute("data-tab");
-            let currentTab = document.querySelector(tabId);
 
 
-            if (currentBtn.classList.contains("active")) {
-                currentBtn.classList.remove("active");
-                currentTab.classList.remove("active");
-            } else {
-                tabBtn.forEach(function (item) {
-                    item.classList.remove("active");
-                });
-                tabsItems.forEach(function (item) {
-                    item.classList.remove("active");
-                });
 
-                currentBtn.classList.add("active");
-                currentTab.classList.add("active");
-            }
-        });
+const tabBtn = document.querySelectorAll('.tab-btn');
+const tabsItems = document.querySelectorAll('.tab-item');
+
+tabBtn.forEach(onTabClick)
+
+function onTabClick(item) {
+  item.addEventListener("click", function () {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
+
+
+    if (currentBtn.classList.contains("active")) {
+      currentBtn.classList.remove("active");
+      currentTab.classList.remove("active");
+    } else {
+      tabBtn.forEach(function (item) {
+        item.classList.remove("active");
+      });
+      tabsItems.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      currentBtn.classList.add("active");
+      currentTab.classList.add("active");
+    }
+  });
+}
+
+
+let form = document.getElementById('form');
+let client = document.getElementById('client');
+let mail = document.getElementById('mail');
+let link = document.getElementById('link');
+let question = document.getElementById('question');
+let success = document.getElementById('success');
+let danger = document.getElementById('danger');
+let close = document.getElementById('close');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  function checkInputs() {
+    let mailValue = mail.value.trim();
+    let clientValue = client.value.trim();
+    let linkValue = link.value.trim();
+    let questionValue = question.value.trim();
+
+    if (mailValue === '' || clientValue === '' || linkValue === '' || questionValue === '') {
+      danger.style.display = 'block';
+    } else {
+      setTimeout(() => {
+        mailValue = '';
+        clientValue = '';
+        linkValue = '';
+        questionValue = '';
+      }, 2000);
+      danger.style.display = 'none';
+      success.classList.add('popup-form__fixed--active');
     }
 
+    if (success.classList.contains('popup-form__fixed--active')) {
+      close.addEventListener("click", function () {
+        success.classList.remove('popup-form__fixed--active')
+      });
+    }
 
-
-
-
+  }
+  checkInputs();
+});
