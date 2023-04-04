82fe2677d74d0e54806278eb1f985e45bc30fcc8
Denys <den.popov1002@gmail.com>
2023-04-04 12:55:18 +0300

    blog-adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,134 @@
 
+
+/////////////////blog page swiper/////////////////////
+const InfoSwiper = new Swiper('.swiper-info', {
+  // Optional parameters
+  direction: 'horizontal',
+  spaceBetween: 33,
+  slidesPerView: 3,
+  loop: false,
+  freeMode: true,
+
+  navigation: {
+    nextEl: '.blog-info__arrow-next',
+    prevEl: '.blog-info__arrow-prev',
+  },
+
+  breakpoints: {// настройки для разных разрешений
+    360: {
+      slidesPerView: 1.5,
+      centeredSlides: true,
+      initialSlide: 1,
+      spaceBetween: 20,
+    },
+    380: {
+      slidesPerView: 1.8,
+      spaceBetween: 20,
+      centeredSlides: true,
+      initialSlide: 1,
+    },
+    460: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    500: {
+      slidesPerView: 2.3,
+      spaceBetween: 30,
+    },
+    560: {
+      slidesPerView: 2.2,
+      spaceBetween: 30,
+    },
+    660: {
+      slidesPerView: 2.5,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 60,
+    },
+    800: {
+      slidesPerView: 2.2,
+      spaceBetween: 60,
+    },
+    900: {
+      slidesPerView: 2.5,
+      spaceBetween: 60,
+    },
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 60,
+    },
+    1200: {
+      slidesPerView: 3,
+    }
+  }
+});
+
+const InstSwiper = new Swiper('.swiper-inst', {
+  // Optional parameters
+  direction: 'horizontal',
+  spaceBetween: 33,
+  slidesPerView: 3,
+  loop: false,
+  freeMode: true,
+
+  navigation: {
+    nextEl: '.blog-inst__arrow-next',
+    prevEl: '.blog-inst__arrow-prev',
+  },
+
+  breakpoints: {// настройки для разных разрешений
+    360: {
+      slidesPerView: 1.5,
+      centeredSlides: true,
+      initialSlide: 1,
+      spaceBetween: 20,
+    },
+    380: {
+      slidesPerView: 1.8,
+      spaceBetween: 20,
+      centeredSlides: true,
+      initialSlide: 1,
+    },
+    460: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    500: {
+      slidesPerView: 2.3,
+      spaceBetween: 30,
+    },
+    560: {
+      slidesPerView: 2.2,
+      spaceBetween: 30,
+    },
+    660: {
+      slidesPerView: 2.5,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 60,
+    },
+    800: {
+      slidesPerView: 2.2,
+      spaceBetween: 60,
+    },
+    900: {
+      slidesPerView: 2.5,
+      spaceBetween: 60,
+    },
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 60,
+    },
+    1200: {
+      slidesPerView: 3,
+    }
+  }
+});
+//////////////////////////////////////////////
 // const swiperContainer = document.querySelector('.swiper-container');
 // const swiperWrapper = swiperContainer.querySelector('.swiper-wrapper');
 // const swiperSlides = swiperWrapper.querySelectorAll('.swiper-slide');
@@ -156,3 +286,6 @@ form.addEventListener('submit', (e) => {
   }
   checkInputs();
 });
+
+
+
